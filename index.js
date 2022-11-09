5a9a25471a1d688e4b008aba2dd7c4959fa2245c
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-11-09 17:08:38 +0600

    my review done

--- index.js
+++ index.js
@@ -38,18 +38,18 @@ async function run() {
       res.send(result);
     });
 
-    //review section
-    // app.get("/reviews", async (req, res) => {
-    //   let query = {};
-    //   if (req.query.email) {
-    //     query = {
-    //       email: req.query.email,
-    //     };
-    //   }
-    //   const cursor = reviewCollection.find(query);
-    //   const reviews = await cursor.toArray();
-    //   res.send(reviews);
-    // });
+    // review section
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
     app.get("/reviews", async (req, res) => {
       let query = {};
       if (req.query.title) {
