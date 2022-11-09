7158472a82af32c30fc3ade92349b8739c4e25bc
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-11-09 17:26:04 +0600

    specific data load done

--- index.js
+++ index.js
@@ -55,17 +55,6 @@ async function run() {
       const reviews = await cursor.toArray();
       res.send(reviews);
     });
-    // app.get("/reviews", async (req, res) => {
-    //   let query = {};
-    //   if (req.query.title) {
-    //     query = {
-    //       title: req.query.title,
-    //     };
-    //   }
-    //   const cursor = reviewCollection.find(query);
-    //   const reviews = await cursor.toArray();
-    //   res.send(reviews);
-    // });
     app.post("/reviews", async (req, res) => {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
